5e836b241f94e41707a07ef399727de8333fb37e
joqim <jo@joqim.com>
2023-06-14 17:11:13 -0600

    logo

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
     <aside className="md:w-[150px] md:flex-shrink-0 -mx-4 md:mx-0 md:px-0 font-serif">
       <div className="lg:sticky lg:top-20">
         <div className="ml-2 md:ml-[12px] mb-2 px-4 md:px-0 md:mb-8 space-y-10 flex flex-col md:flex-row items-start ">
-        <Link aria-label="Lee Robinson" href="/">
+        {/* <Link aria-label="Lee Robinson" href="/">
           <motion.img
             className="h-[25px] md:h-[37px]"
             // src={SVG.src}
@@ -47,7 +47,7 @@ export default function Navbar() {
               stiffness: 50,
             }}
           />
-        </Link>
+        </Link> */}
         </div>
         <LayoutGroup>
           <nav
