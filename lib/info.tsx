6164026641da66ed3c13b6aa1c2bbca14e6e4ae5
joqim <jo@joqim.com>
2023-06-20 13:02:11 -0600

    testing out branch

--- lib/info.tsx
+++ lib/info.tsx
@@ -12,8 +12,7 @@ export const about = () => {
 export const bio = () => {
   return (
     <>
-      I'm a Next.js enthusiast and contribute immensely to open-source communities. I'm
-      passionate about frontend development. I strive to push the boundaries of web experiences and empower users through elegant, efficient, and accessible interfaces. 
+      I'm a Next.js enthusiast and contribute immensely to open-source communities. I'm passionate about frontend development. I strive to push the boundaries of web experiences and empower users through elegant, efficient, and accessible interfaces. 
     </>
   );
 };
